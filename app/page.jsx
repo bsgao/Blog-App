b46876f6761c9d6734ed5684d38a9746d1eec062
bsgao <53946585+bsgao@users.noreply.github.com>
2023-11-29 20:09:52 -0500

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TicketCard from "./(components)/TicketCard";
+
+const Dashboard = () => {
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-6 ">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
